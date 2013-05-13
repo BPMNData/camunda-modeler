049e7e3633ae201f5cdb723cea4e24ee3b1ba357
Sebastian Kruse <s.t.kruse@gmx.de>
2013-05-13 14:19:59 +0200

    Moved primary and foreign key properties to own property panel. Grayed out primary key type when there is no primary key.

--- org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/ui/property/PropertiesTabDescriptorProvider.java
+++ org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/ui/property/PropertiesTabDescriptorProvider.java
@@ -27,8 +27,10 @@ import org.camunda.bpm.modeler.ui.property.tabs.EventTabSection;
 import org.camunda.bpm.modeler.ui.property.tabs.GeneralTabSection;
 import org.camunda.bpm.modeler.ui.property.tabs.ListenerTabSection;
 import org.camunda.bpm.modeler.ui.property.tabs.MultiInstanceTabSection;
+import org.camunda.bpm.modeler.ui.property.tabs.RelationalKeysTabSection;
 import org.camunda.bpm.modeler.ui.property.tabs.ScopeInformationTabSection;
 import org.eclipse.bpmn2.Activity;
+import org.eclipse.bpmn2.DataObject;
 import org.eclipse.bpmn2.Event;
 import org.eclipse.bpmn2.Gateway;
 import org.eclipse.bpmn2.Participant;
@@ -45,92 +47,112 @@ import org.eclipse.ui.views.properties.tabbed.ITabDescriptorProvider;
 
 public class PropertiesTabDescriptorProvider implements ITabDescriptorProvider {
 
-	@Override
-	public ITabDescriptor[] getTabDescriptors(IWorkbenchPart part, ISelection selection) {
-		List<ITabDescriptor> tabs = new ArrayList<ITabDescriptor>();
-		
-		Object model = getSelectedModel(selection);
-		EObject businessObject = getSelectedBusinessObject(model);
-		
-		if (businessObject != null) {
-			tabs.add(createGeneralTabDescriptor());
-			
-			if (businessObject instanceof Event) {
-				tabs.add(createEventTabDescriptor());
-			}
-			
-			// add definitions tab for errors, messages and signals
-			if (model instanceof Diagram) {
-				tabs.add(createDefinitionsTabDescriptor());
-			}
-			
-			// add multi instance tabs to activities
-			if (businessObject instanceof Activity) {
-				tabs.add(createMultiInstanceTabDescriptor());
-			}
-			
-			// add execution listener tabs
-			if (businessObject instanceof Activity ||
-				businessObject instanceof Gateway ||
-				businessObject instanceof SequenceFlow || 
-				businessObject instanceof Event || 
-				businessObject instanceof Process) {
-				
-				tabs.add(createListenerTabDescriptor());
-			}
-			
-			// for participant, too
-			if (businessObject instanceof Participant) {
-				Participant participant = (Participant) businessObject;
-				
-				if (participant.getProcessRef() != null) {
-					tabs.add(createListenerTabDescriptor());
-					
-					// BPMNData ScopeInformation property
-					tabs.add(createScopeInformationTabDescriptor());
-				}
-			}
-			
-			// BPMNData ScopeInformation property
-			if (businessObject instanceof Process || businessObject instanceof SubProcess) {
-				tabs.add(createScopeInformationTabDescriptor());
-			}
-		}
-		
-		return tabs.toArray(new ITabDescriptor[]{});
-	}
-	
-	private ITabDescriptor createTabDescriptor(String id, String name, AbstractTabSection tabSection) {
+    @Override
+    public ITabDescriptor[] getTabDescriptors(IWorkbenchPart part,
+            ISelection selection) {
+        List<ITabDescriptor> tabs = new ArrayList<ITabDescriptor>();
 
-		TabDescriptor tabDescriptor = new TabDescriptor(id, name, name);
-		ISectionDescriptor sectionDescriptor = new SectionDescriptor(id + ".section", tabSection);
-		tabDescriptor.setSectionDescriptors(Arrays.asList(new ISectionDescriptor[] { sectionDescriptor }));
-		
-		return tabDescriptor;
-	}
+        Object model = getSelectedModel(selection);
+        EObject businessObject = getSelectedBusinessObject(model);
 
-	private ITabDescriptor createMultiInstanceTabDescriptor() {
-		return createTabDescriptor("multiInstanceTab", "Multi Instance", new MultiInstanceTabSection());
-	}
+        if (businessObject != null) {
+            tabs.add(createGeneralTabDescriptor());
 
-	private ITabDescriptor createEventTabDescriptor() {
-		return createTabDescriptor("eventTab", "Event", new EventTabSection());
-	}
-	
-	private ITabDescriptor createListenerTabDescriptor() {
-		return createTabDescriptor("listenerTab", "Listener", new ListenerTabSection());
-	}
+            if (businessObject instanceof Event) {
+                tabs.add(createEventTabDescriptor());
+            }
 
-	private ITabDescriptor createGeneralTabDescriptor() {
-		return createTabDescriptor("generalTab", "General", new GeneralTabSection());
-	}
+            // add definitions tab for errors, messages and signals
+            if (model instanceof Diagram) {
+                tabs.add(createDefinitionsTabDescriptor());
+            }
 
-	private ITabDescriptor createDefinitionsTabDescriptor() {
-		return createTabDescriptor("definitionsTab", "Definitions", new DefinitionsTabSection());
-	}
-	
-	private ITabDescriptor createScopeInformationTabDescriptor() {
-		return createTabDescriptor("scopeInformationTab", "BPMN Data Scope", new ScopeInformationTabSection());
-	}
+            // add multi instance tabs to activities
+            if (businessObject instanceof Activity) {
+                tabs.add(createMultiInstanceTabDescriptor());
+            }
+
+            // add execution listener tabs
+            if (businessObject instanceof Activity
+                    || businessObject instanceof Gateway
+                    || businessObject instanceof SequenceFlow
+                    || businessObject instanceof Event
+                    || businessObject instanceof Process) {
+
+                tabs.add(createListenerTabDescriptor());
+            }
+
+            // for participant, too
+            if (businessObject instanceof Participant) {
+                Participant participant = (Participant) businessObject;
+
+                if (participant.getProcessRef() != null) {
+                    tabs.add(createListenerTabDescriptor());
+
+                    // BPMNData ScopeInformation property
+                    tabs.add(createScopeInformationTabDescriptor());
+                }
+            }
+
+            // BPMNData ScopeInformation property
+            if (businessObject instanceof Process
+                    || businessObject instanceof SubProcess) {
+                tabs.add(createScopeInformationTabDescriptor());
+            }
+
+            // BPMNData Primary and foreign keys
+            if (businessObject instanceof DataObject) {
+                tabs.add(createRelationKeysTabDescriptor());
+            }
+        }
+
+        return tabs.toArray(new ITabDescriptor[] {});
+    }
+
+    private ITabDescriptor createTabDescriptor(String id, String name,
+            AbstractTabSection tabSection) {
+
+        TabDescriptor tabDescriptor = new TabDescriptor(id, name, name);
+        ISectionDescriptor sectionDescriptor = new SectionDescriptor(id
+                + ".section", tabSection);
+        tabDescriptor.setSectionDescriptors(Arrays
+                .asList(new ISectionDescriptor[] { sectionDescriptor }));
+
+        return tabDescriptor;
+    }
+
+    private ITabDescriptor createMultiInstanceTabDescriptor() {
+        return createTabDescriptor("multiInstanceTab", "Multi Instance",
+                new MultiInstanceTabSection());
+    }
+
+    private ITabDescriptor createEventTabDescriptor() {
+        return createTabDescriptor("eventTab", "Event", new EventTabSection());
+    }
+
+    private ITabDescriptor createListenerTabDescriptor() {
+        return createTabDescriptor("listenerTab", "Listener",
+                new ListenerTabSection());
+    }
+
+    private ITabDescriptor createGeneralTabDescriptor() {
+        return createTabDescriptor("generalTab", "General",
+                new GeneralTabSection());
+    }
+
+    private ITabDescriptor createDefinitionsTabDescriptor() {
+        return createTabDescriptor("definitionsTab", "Definitions",
+                new DefinitionsTabSection());
+    }
+
+    private ITabDescriptor createScopeInformationTabDescriptor() {
+        return createTabDescriptor("scopeInformationTab", "BPMN Data Scope",
+                new ScopeInformationTabSection());
+    }
+
+    private ITabDescriptor createRelationKeysTabDescriptor() {
+        return createTabDescriptor("relationalKeysTab", "BPMN Data Keys",
+                new RelationalKeysTabSection());
+    }
 
 }
